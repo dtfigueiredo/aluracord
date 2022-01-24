65a69955ccc1c17b2d2f213da8eef14d05388c64
Daniel Figueiredo <dt.figueiredo90@gmail.com>
2022-01-24 13:22:30 -0300

    upload arquivos iniciais

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Button from '../components/button'
+
+const HomePage = () => {
+  return (
+    <div >
+      <h1>Testing</h1>
+      <Button
+        name='Cadastrar-se' />
+      <a>Link</a>
+    </div >
+  );
+};
+
+export default HomePage;
