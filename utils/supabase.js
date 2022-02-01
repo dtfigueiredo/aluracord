b19493d807549370a736c37de88747d2d7f2247c
Daniel Figueiredo <dt.figueiredo90@gmail.com>
2022-02-01 10:57:16 -0300

    removendo utils do gitignore

---
+++ utils/supabase.js
@@ -0,0 +1,4 @@
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQxMjQ0NCwiZXhwIjoxOTU4OTg4NDQ0fQ.aLlRN75ECKmJW2t195U9NM_5nJZ2f9BXxgYiPh1-HIw'
+
+export const supabaseUrl = 'https://jbbmuwmqhxsopmfzcynf.supabase.co'
+
